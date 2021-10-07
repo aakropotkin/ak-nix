264ee2d793fc57588054f418e51146d8ec67a182
aakropotkin <alex.ameen.tx@gmail.com>
2021-10-06 23:27:38 -0500

    move ini2json to repo

--- flake.nix
+++ flake.nix
@@ -4,18 +4,21 @@
   inputs.nixpkgs.follows = "nix/nixpkgs";
   inputs.set_wm_class.url = "github:aakropotkin/set_wm_class";
   inputs.ak-core.url = "github:aakropotkin/ak-core";
+  inputs.ini2json.url = "github:aakropotkin/ini2json";
 
-  outputs = { self, nixpkgs, set_wm_class, ak-core, nix, ... }: {
+  outputs = { self, nixpkgs, set_wm_class, ak-core, ini2json, nix, ... }: {
 
     packages.x86_64-linux =
       set_wm_class.packages.x86_64-linux //
-      ak-core.packages.x86_64-linux;
+      ak-core.packages.x86_64-linux //
+      ini2json.packages.x86_64-linux;
 
     overlays.set_wm_class = set_wm_class.overlay;
     overlays.ak-core = ak-core.overlay;
     overlays.ak-nix = final: prev:
       ( set_wm_class.overlay final prev ) //
-      ( ak-core.overlay final prev );
+      ( ak-core.overlay final prev ) //
+      ( ini2json.overlay final prev );
     overlay = self.overlays.ak-nix;
 
     nixosModules.set_wm_class = set_wm_class.nixosModule;
