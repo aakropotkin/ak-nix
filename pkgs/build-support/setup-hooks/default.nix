e6532225c40c06566b91e40f1a54b4ddf90ae0b1
Alex Ameen <alex.ameen.tx@gmail.com>
2021-12-12 17:07:48 -0600

    made isAR.sh available as a standalone script

--- pkgs/build-support/setup-hooks/default.nix
+++ pkgs/build-support/setup-hooks/default.nix
@@ -1,4 +1,8 @@
-{ makeSetupHook, ... }:
+{ makeSetupHook, writeShellScriptBin, ... }:
 {
   isAR = makeSetupHook { name = "isAR"; } ./isAR.sh;
+  isAR_sh = let isARContents = builtins.readFile ./isAR.sh; in
+    writeShellScriptBin "isAR.sh" ( isARContents + ''
+      isAR "$1"
+    '' );
 }
