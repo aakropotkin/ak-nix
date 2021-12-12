a77ce627281ddbea82fcd30e08710750bee1d842
Alex Ameen <alex.ameen.tx@gmail.com>
2021-12-12 16:23:41 -0600

    add inline comment about Zsh vs Bash compatibility for 'read -n' and 'read -k' builtins

--- pkgs/build-support/setup-hooks/isAR.sh
+++ pkgs/build-support/setup-hooks/isAR.sh
@@ -3,8 +3,15 @@ function isAR() {
   local fn="$1"
   local fd
   local magic
-  exec {fd}< "$fn"
+  exec {fd}<"$fn"
+  ### In Zsh `read -k NUM' behaves like Bash's `read -n NUM'.
+  ##if test "${ZSH_VERSION+set}" = set; then
+  ##  read -r -k 7 -u "$fd" magic
+  ##else
+  ##  read -r -n 7 -u "$fd" magic
+  ##fi
   read -r -n 7 -u "$fd" magic
   exec {fd}<&-
+  # First seven characters should be "!<arch>"
   if test "$magic" = $'\041\074arch\076'; then return 0; else return 1; fi
 }
