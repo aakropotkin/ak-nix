9c55097c2bdc0d62962ec780878980123172d449
Alex Ameen <alex.ameen.tx@gmail.com>
2022-06-11 13:51:46 -0500

    update flake locks

--- lib/tests/debug.nix
+++ lib/tests/debug.nix
@@ -1,14 +1,18 @@
+
+# Test for your testers so we can test while we test!
+
 { lib           ? ( builtins.getFlake ( toString ../.. ) ).lib
 , nixpkgs       ? builtins.getFlake "nixpkgs"
 , system        ? builtins.currentSystem
 , pkgs          ? nixpkgs.legacyPackages.${system}
 , writeText     ? pkgs.writeText
-}:
-let
+}: let
+
   inherit (lib) libdbg;
 
 /* -------------------------------------------------------------------------- */
 
+  # Tests for our tests.
   innerTests = {
     testMaths = { expr = 1 + 1; expected = 2; };
     testStrings = { expr = builtins.substring 0 1 "foo"; expected = "f"; };
@@ -19,19 +23,25 @@ let
 
   tests = {
 
-    testRunner = {
-      expr     = lib.runTests innerTests;
-      expected = [];
-    };
-
+    # Sanity check the fields that `nixpkgs.lib.runTests' includes in its
+    # list of failure cases.
     testRunTestsFields = {
       expr = let
         run = lib.runTests { testFail = { expr = 1; expected = 2; }; };
         fields = builtins.attrNames ( builtins.head run );
       in fields;
+      # The order of these matters, `attrNames' is alphabetically sorted.
       expected = ["expected" "name" "result"];
     };
 
+    # Sanity check that `nixpkgs.lib.runTests' returns an empty list when all
+    # tests pass.
+    testRunner = {
+      expr     = lib.runTests innerTests;
+      expected = [];
+    };
+
+    # Sanity check that `libdbg.checker' returns `true' when all tests pass.
     testChecker = {
       expr = libdbg.checker "tests" ( lib.runTests innerTests );
       expected = true;
@@ -42,9 +52,22 @@ let
 
 /* -------------------------------------------------------------------------- */
 
+  # Use the test harness as our driver.
+  # FIXME:
+  # Honestly this should be a test case that is driven by a dead simple driver,
+  # but I'm feeling lazy and this at leasts proves that `mkTestHarness'
+  # is free of syntax errors and performs it's basic functions.
+  #
+  # The problem really being: if the test harness is broken in a way that
+  # doesn't properly detect errors in tests, then it cannot be expected to
+  # properly report its own flaws.
+  # This is a "who polices the police?" paradox.
   harness = libdbg.mkTestHarness {
     inherit writeText tests;
     env = { inherit lib libdbg system nixpkgs pkgs tests innerTests; };
   };
+
+
+/* -------------------------------------------------------------------------- */
 
 in harness
