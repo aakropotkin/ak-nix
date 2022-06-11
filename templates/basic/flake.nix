146c012695628cedcbd6237ca09658d6773f046c
Alex Ameen <alex.ameen.tx@gmail.com>
2022-06-11 14:05:17 -0500

    patch basic template

--- templates/basic/flake.nix
+++ templates/basic/flake.nix
@@ -1,35 +1,46 @@
 {
   description = "a basic package";
 
-  inputs.utils.url = github:numtide/flake-utils;
-  inputs.@PROJECT@-src.url = {
-    url = github:@OWNER@/@PROJECT@;
-    flake = false;
-  };
+  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.utils.inputs.nixpkgs.follows = "/nixpkgs";
 
-  outputs = { self, nixpkgs, utils, @PROJECT@-src }:
-  let systemMap = utils.lib.eachSystemMap utils.lib.defaultSystems;
+  inputs.basic-src.url   = "github:owner/basic";
+  inputs.basic-src.flake = false;
+
+  outputs = { self, nixpkgs, utils, basic-src }: let
+    inherit (utils.lib) eachDefaultSystemMap;
   in {
-    packages = systemMap ( system:
-      let pkgsFor = import nixpkgs { inherit system; };
-      in rec {
-        @PROJECT@ = pkgsFor.stdenv.mkDerivation {
-          pname = "@PROJECT@";
-          version = "@VERSION@";
-          src = @PROJECT@-src;
-          nativeBuildInputs = with pkgsFor; [
-            # pkg-config
-            # help2man
-            # texinfoInteractive
-            autoreconfHook
-          ];
-          buildInputs = with pkgsFor; [
-            # zlib.dev
-          ];
-        };
-        default = @PROJECT@;
-      }
-    );
+
+    packages = eachDefaultSystemMap ( system: let
+      pkgsFor = nixpkgs.legacyPackages.${system};
+    in {
+      basic = pkgsFor.stdenv.mkDerivation {
+        pname = "basic";
+        version = "0.0.0";
+        src = basic-src;
+        nativeBuildInputs = with pkgsFor; [
+          # pkg-config
+          # help2man
+          # texinfoInteractive
+          autoreconfHook
+        ];
+        buildInputs = with pkgsFor; [
+          # zlib.dev
+        ];
+      };
+
+      default = self.packages.${system}.basic;
+    } );  # End Packages
+
   };
 }
-
+# NOTE: This approach to building is not compatible with overlays.
+# This is fine for the vast majority of use cases, but if you need to add
+# your package to the `nixpkgs.legacyPackages' set you'll need to write
+# an overlay which references `nixpkgs' by `final' and `prev'.
+# Cases where the overlay is useful are:
+#   1. I want `stdenv' modifiers such as `pkgsCross' and `pkgsStatic' to work
+#      on my package.
+#   2. My package is a module for an interpreter, and I want it to be
+#      available globally for functions like `mk<Lang>Package'.
+#      This largely effects Python and Haskell.
