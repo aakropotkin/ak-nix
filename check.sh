8a4e7006550ebaccdffc362a4d3aedffd85b321b
Alex Ameen <alex.ameen.tx@gmail.com>
2022-10-11 18:03:31 -0500

    cleanup in check.sh

--- check.sh
+++ check.sh
@@ -9,5 +9,6 @@ $NIX flake check $NIX_FLAGS;
 $NIX flake check $NIX_FLAGS --impure;
 $NIX flake check $NIX_FLAGS --system "$SYSTEM";
 $NIX flake check $NIX_FLAGS --system "$SYSTEM" --impure;
+trap '_es="$?"; rm -f ./result; exit "$_es";' HUP TERM EXIT INT QUIT;
 $NIX build .#tests $NIX_FLAGS;
 $NIX build .#tests $NIX_FLAGS --impure;
