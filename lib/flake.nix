45c921730436200aae2197ce48005c4d0b0616b3
Alex Ameen <alex.ameen.tx@gmail.com>
2022-10-19 20:04:03 -0500

    drop yants input

--- lib/flake.nix
+++ lib/flake.nix
@@ -4,11 +4,6 @@
   inputs.nix.url = "github:NixOS/nix";
   inputs.nix.inputs.nixpkgs.follows = "/nixpkgs";
 
-  inputs.yants-src = {
-    url = "git+https://code.tvl.fyi/depot.git:/nix/yants.git";
-    flake = false;
-  };
-  
   outputs = { self, nixpkgs, nix, yants-src, ... }: {
     # An extension to `nixpkgs.lib'.
     lib = import ./. { inherit nix yants-src; inherit (nixpkgs) lib; };
